451165b7318ae910434db9b3313ac18ec006ec8f
Roadkill43 <marty@Halls-MBP.Martyrhall.com>
2017-08-08 22:08:45 -0700

    Updates to script

--- mypicasa.py
+++ mypicasa.py
@@ -12,6 +12,7 @@ import tempfile
 import calendar
 import httplib2
 import threading
+import requests
 
 # from apiclient import discovery
 from oauth2client import client
@@ -61,17 +62,67 @@ def refreshCreds(credentials,sleep):
 def flatten(name):
         return re.sub("#[0-9]*$", "", name).rstrip()
 
-def scanWebPhotos(webAlbum):
-	photos = repeat(lambda: gd_client.GetFeed(webAlbum.GetPhotosUri() + "&imgmax=d"),
-		"list photos in album", True)
-	for photo in photos.entry:
-		photoDate = datetime.fromtimestamp(int(photo.timestamp.text[0:10])).strftime('%Y/%m/%d')
-		print photo.size.text 
-        	photoTitle = urllib.unquote(photoDate + "/" + photo.title.text)
-		print photoTitle
-		file = FileEntry(photo.title.text, photoTitle, None, False, photo)
-		file.print_entry()
+# determine what webphotos exist are 
+def scanWebPhotos(webAlbum, rootDirs, webAlbumTitle):
+    photos = repeat(lambda: gd_client.GetFeed(webAlbum.GetPhotosUri() + "&imgmax=d"), "list photos in album", True)
+    for photo in photos.entry:
+        photoDate = datetime.fromtimestamp(int(photo.timestamp.text[0:10])).strftime('%Y/%m/%d')
+        photoTitle = urllib.unquote(photoDate + "/" + photo.title.text)
 
+        localPhotoPath = rootDirs[0] + "/"
+        file = RemoteFileEntry(photo.title.text, localPhotoPath, photo, photoDate, webAlbumTitle)
+
+        if verbose or debug:
+                print " %s -" % photoTitle,
+                if debug:
+                    print localPhotoPath
+                    file.print_entry()
+
+        # if the entry doesn't exist yet, add it. other wise, skip
+        entryCaptured = False
+        iteration = 0
+        while not entryCaptured:
+            iteration += 1
+            if (not photoTitle in onlineEntries):
+                onlineEntries[photoTitle] = file
+                entryCaptured = True
+                if verbose or debug:
+                    print "added to list"
+            else:
+                if file.getAlbum() == onlineEntries[photoTitle].getAlbum() and file.getHash() != onlineEntries[photoTitle].getHash():
+                    # add with an additiona (1) in the name
+                    file.updateName(iteration)
+                    photoTitle = urllib.unquote(photoDate + "/" + file.getName())
+                    if verbose or debug:
+                        print "Dup, renaming to %s" % photoTitle
+
+                elif file.getAlbum() == "" and onlineEntries[photoTitle].getAlbum() != "" and file.getHash() == onlineEntries[photoTitle].getHash():
+                    # skip entry
+                    entryCaptured = True
+                    if verbose or debug:
+                        print "already exists in album"
+                elif file.getAlbum() != "" and file.getAlbum() != onlineEntries[photoTitle].getAlbum() and file.getHash() == onlineEntries[photoTitle].getHash():
+                    #file exists in multiple albums
+                    entryCaptured = True
+                    photoTitle = urllib.unquote(file.getAlbum() + photoDate + "/" + file.getName())
+                    onlineEntries[photoTitle] = file
+                    entryCaptured = True
+                    if verbose or debug:
+                        print "added to list, new album"
+                else:
+                    entryCaptured = True
+                    print "error, shouldn't happen"
+
+
+                
+# Download the photos from online
+def downloadWebPhotos():
+    # download files
+    for photoTitle in onlineEntries:
+        file = onlineEntries[photoTitle]
+        file.download_file()
+
+# repeat function
 def repeat(function,  description, onFailRethrow):
 	exc_info = None
 	for attempt in range(3):
@@ -86,7 +137,6 @@ def repeat(function,  description, onFailRethrow):
 			time.sleep(6)
 			# this should no longer be needed
 			# gd_client=oauthLogin()
-
 			continue
 		else:
 			break
@@ -94,9 +144,10 @@ def repeat(function,  description, onFailRethrow):
 		print ("WARNING: Failed to %s. This was due to %s" % (description, exc_info))
 		if onFailRethrow:
 			raise exc_info
+
 # Class to store details of an individual file
-class FileEntry:
-    def __init__(self, name, path, webReference, isLocal, album):
+class RemoteFileEntry:
+    def __init__(self, name, path, webReference, datePath, album):
         self.name = name
         if path:
             self.path = path
@@ -104,14 +155,29 @@ class FileEntry:
         else:
             self.path = os.path.join(album.rootPath, name)
             self.type = None
-        self.isLocal = isLocal
-        self.localHash = None
-        self.remoteHash = None
         self.remoteDate = None
         self.remoteTimestamp = None
         self.remoteSize = None
         self.album = album
+        self.datePath = datePath
         self.setWebReference(webReference)
+
+    def getAlbum(self):
+        return self.album
+
+    def getName(self):
+        return self.name
+
+    def getHash(self):
+        return self.hash
+
+    def getAlbumHash(self):
+        return self.albumHash
+
+    def updateName(self,Number):
+        newName = self.name.rsplit('.',1)[0] +" ("+str(Number)+")."+self.name.rsplit('.',1)[1]
+        self.name = newName
+        self.albumHash = hash(str(self.album) + str(self.name) + str(self.remoteSize) + str(self.remoteTimestamp))
 
     def setWebReference(self, webReference):
         if webReference:
@@ -131,28 +197,88 @@ class FileEntry:
                     if highRescontent.url:
                         self.webUrl = highRescontent.url
 
-            self.remoteHash = webReference.checksum.text
             self.remoteDate = calendar.timegm(
                 time.strptime(re.sub("\.[0-9]{3}Z$", ".000 UTC", webReference.updated.text),
                               '%Y-%m-%dT%H:%M:%S.000 %Z'))
             self.remoteTimestamp = time.mktime(webReference.timestamp.datetime().timetuple())
             self.remoteSize = int(webReference.size.text)
+            self.hash = hash(str(self.name) + str(self.remoteSize) + str(self.remoteTimestamp))
+            self.albumHash = hash(str(self.album) + str(self.name) + str(self.remoteSize) + str(self.remoteTimestamp))
         else:
             self.webUrl = None
 
     def print_entry(self):
-	print self.webUrl
-	print self.isLocal
-       	print self.localHash
-       	print self.remoteHash
-       	print self.remoteDate
-       	print self.remoteTimestamp 
-       	print self.remoteSize 
-       	print self.album 
+        print ("name %s" % self.name)
+        print ("datePath %s" % self.datePath)
+        print ("album %s" % self.album)
+        print ("albumid %s" % self.albumid)
+        print ("path %s" % self.path)
+        print ("webUrl %s" % self.webUrl)
+        print ("Remote date %s" % self.remoteDate)
+        print ("RemoteTimesamp %s" % self.remoteTimestamp) 
+        print ("Remote size %s" % self.remoteSize)
+
+    def download_file(self):
+        url = self.webUrl
+        if self.album == "":
+            local_filename = self.path + self.datePath+ "/" + self.name
+            localPath = self.path + self.datePath
+        else:
+            local_filename = self.path + self.album + "/" + self.name
+            localPath = self.path + self.album
+            
+        #check to see if the file exists already locally
+        if os.path.isfile(local_filename):
+            if verbose or debug:
+                print "Checking...%s already exists locally" % local_filename
+            self.localSize = os.path.getsize(local_filename)
+            self.localDate = os.path.getmtime(local_filename)
+            self.localDate = self.localDate
+        else:
+            self.localSize = 0
+            self.localDate = 0
+        #check for date or size difference
+        if self.localDate != self.remoteTimestamp or (self.type =='photo' and self.localSize != self.remoteSize):
+            if verbose or debug:
+                print "downloading %s" % local_filename
+                if debug:
+                    print "---Local Date %s, Remote Date %s ---" % (self.localDate, self.remoteTimestamp)
+                    print "---Local size %s, Remote size %s ---" % (self.localSize, self.remoteSize)
+                    print "-- Type %s" % self.type
+            # NOTE the stream=True parameter
+            if not test:
+                r = requests.get(url, stream=True)
+                if not os.path.exists(localPath):
+                        os.makedirs(localPath)
+                with open(local_filename, 'wb') as f:
+                    for chunk in r.iter_content(chunk_size=24 * 1024): 
+                        if chunk: # filter out keep-alive new chunks
+                            f.write(chunk)
+                            #f.flush() commented by recommendation from J.F.Sebastian
+                os.utime(local_filename, (int(self.remoteTimestamp), int(self.remoteTimestamp)))
+                self.localSize = os.path.getsize(local_filename)
+                self.localDate = os.path.getmtime(local_filename)
+                self.localDate = self.localDate
+                #confirm the download was succesful
+                if self.localDate == self.remoteTimestamp and (self.localSize == self.remoteSize or self.type =='video'):
+                    if verbose:
+                        print "Success: download of %s completed" % local_filename
+                else:
+                    if verbose or debug:
+                        print "Error when downloading %s" % local_filename
+                        if debug: 
+                                print "---Local Date %s, Remote Date %s ---" % (self.localDate, self.remoteTimestamp)
+                                print "---Local size %s, Remote size %s ---" % (self.localSize, self.remoteSize)
+            else:
+                print "Test mode -file %s to be downloaded" % local_filename
+        else:
+            if verbose or debug:
+                print "skipping File %s already downloaded" % local_filename
 
 
-
+# -------------------------------#
 # Main Program code
+# -------------------------------#
 parser = argparse.ArgumentParser()
 parser.add_argument("-d", "--directory", nargs='+',
                     help="The local directories. The first of these will be used for any downloaded items")
@@ -161,19 +287,42 @@ parser.add_argument("-t", "--test", default=False, action='store_true',
 parser.add_argument("-o", "--owner", default="default",
                     help="The username of the user whos albums to sync (leave blank for your own)")
 parser.add_argument("-v", "--verbose", default=False, action='store_true', help="Increase verbosity")
+parser.add_argument("-D", "--debug", default=False, action='store_true', help="Debug mode")
+parser.add_argument("-a", "--albums", default=False, action='store_true', help="Albuums only, ignore auto backup, instant upload folders")
 
 args = parser.parse_args()
+# Global data Declerations #
+immutableFolders = frozenset(["Instant Upload","Auto-Backup","Auto Backup"])
+onlineEntries = {}   #online photos/videos
 
+# Arguments
 rootDirs = args.directory  # set the directory you want to sync to
 verbose = args.verbose
+debug = args.debug
+test = args.test
+albumsOnly = args.albums
 
 gd_client = oauthLogin()
 
 # walk the web album finding albums there
 webAlbums = gd_client.GetUserFeed(user=args.owner)
 
+#First check for webalbums, then do immutableFolders
 for webAlbum in webAlbums.entry:
-	webAlbumTitle = flatten(webAlbum.title.text)
-	print ('Scanning web-album %s (containing %s files)' % (webAlbum.title.text, webAlbum.numphotos.text))
-	scanWebPhotos(webAlbum)
+    webAlbumTitle = flatten(webAlbum.title.text)
+    if not webAlbum.title.text in immutableFolders:
+        if verbose or debug:
+            print ('Scanning web-album %s (containing %s files)' % (webAlbum.title.text, webAlbum.numphotos.text))
+        scanWebPhotos(webAlbum, rootDirs, webAlbumTitle)
 
+
+if not albumsOnly:  #skip the immutableFolders if arg albums only is set
+    for webAlbum in webAlbums.entry:
+        webAlbumTitle = flatten(webAlbum.title.text)
+        if webAlbum.title.text in immutableFolders:
+            if verbose or debug:
+                print ('Scanning web-album %s (containing %s files)' % (webAlbum.title.text, webAlbum.numphotos.text))
+            scanWebPhotos(webAlbum, rootDirs, "") # don't pass an album name
+
+
+downloadWebPhotos()
